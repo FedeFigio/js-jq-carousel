4ddc67b683467cdf07d6e42132854de59b439468
Federico <federicofigini90@gmail.com>
2021-03-11 16:14:14 +0100

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,32 +5,29 @@ $(document).ready(function() {
 });
 
 
-
-
 function slide() {
     var btnNext = $(".next")
     var btnPrev = $(".prev")
 
-
     btnNext.click(function(e) {
-        var imgActive = $(".slider-wrapper .active")
+        var active = $(".slider-wrapper .active")
 
-        imgActive.removeClass("active")
-        if (!imgActive.hasClass("last")) {
-            imgActive.next().addClass("active")
+        active.removeClass("active")
+        if (!active.hasClass("last")) {
+            active.next().addClass("active")
         } else {
             $(".first").addClass("active")
         }
     });
-    btnPrev.click(function(e) {
-        var imgActive = $(".slider-wrapper .active")
 
-        imgActive.removeClass("active")
-        if (!imgActive.hasClass("first")) {
-            imgActive.prev().addClass("active")
+    btnPrev.click(function(e) {
+        var active = $(".slider-wrapper .active")
+
+        active.removeClass("active")
+        if (!active.hasClass("first")) {
+            active.prev().addClass("active")
         } else {
             $(".last").addClass("active")
         }
     });
-
 }
